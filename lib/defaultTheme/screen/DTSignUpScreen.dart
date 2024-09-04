42856de7dc5595023e3ff5611ffcd958490882b2
Marcos M. López <marcoslopez1895@gmail.com>
2024-09-03 20:57:44 -0400

    acomodando login

--- lib/defaultTheme/screen/DTSignUpScreen.dart
+++ lib/defaultTheme/screen/DTSignUpScreen.dart
@@ -24,6 +24,7 @@ class DTSignUpScreenState extends State<DTSignUpScreen> {
   var emailCont = TextEditingController(text: 'testing@c.c');
   var passCont = TextEditingController(text: 'sologuillermo');
   var nameCont = TextEditingController();
+  String? messageError;
 
   var emailFocus = FocusNode();
   var passFocus = FocusNode();
@@ -31,8 +32,9 @@ class DTSignUpScreenState extends State<DTSignUpScreen> {
   final LoginController _loginController = LoginController();
 
   Future<void> _login() async {
-    print('Formulario validado');
-
+    setState(() {
+      messageError = null;
+    });
     bool success = await _loginController.login(
       emailCont.text,
       passCont.text,
@@ -47,51 +49,48 @@ class DTSignUpScreenState extends State<DTSignUpScreen> {
         MaterialPageRoute(builder: (context) => SDHomePageScreen()),
       );
     } else {
-      if(mounted){
+      setState(() {
+        messageError = 'Fallo al iniciar sesión. Intente de nuevo';
+      });
+    }
+  }
 
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('Fallo al iniciar sesión. Intente de nuevo')));
-      }
+    @override
+    void initState() {
+      super.initState();
+      init();
     }
 
-  }
+    init() async {
+      //
+    }
 
-  @override
-  void initState() {
-    super.initState();
-    init();
-  }
+    @override
+    void setState(fn) {
+      if (mounted) super.setState(fn);
+    }
 
-  init() async {
-    //
-  }
-
-  @override
-  void setState(fn) {
-    if (mounted) super.setState(fn);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    helper.context = context;
-    return PopScope(
-        canPop: false,
-        child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          //appBar: appBar(context, 'Sign Up'),
-          drawer: DTDrawerWidget(),
-          body: Center(
-            child: Container(
-              width: dynamicWidth(context),
-              child: SingleChildScrollView(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('Iniciar Sesión', style: boldTextStyle(size: 24)),
-                    30.height,
-                    /*TextFormField(
+    @override
+    Widget build(BuildContext context) {
+      helper.context = context;
+      return PopScope(
+          canPop: false,
+          child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            //appBar: appBar(context, 'Sign Up'),
+            drawer: DTDrawerWidget(),
+            body: Center(
+              child: Container(
+                width: dynamicWidth(context),
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Iniciar Sesión', style: boldTextStyle(size: 24)),
+                      30.height,
+                      /*TextFormField(
                   controller: nameCont,
                   style: primaryTextStyle(),
                   decoration: InputDecoration(
@@ -106,90 +105,81 @@ class DTSignUpScreenState extends State<DTSignUpScreen> {
                   onFieldSubmitted: (s) => FocusScope.of(context).requestFocus(emailFocus),
                   textInputAction: TextInputAction.next,
                 ),*/
-                    16.height,
-                    TextFormField(
-                      controller: emailCont,
-                      focusNode: emailFocus,
-                      style: primaryTextStyle(),
-                      decoration: InputDecoration(
-                        labelText: 'Correo Electrónico',
-                        labelStyle: secondaryTextStyle(),
-                        contentPadding: EdgeInsets.all(16),
-                        border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                            borderSide: BorderSide(color: appColorPrimary)),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                            borderSide: BorderSide(
-                                color: appStore.textSecondaryColor!)),
+                      16.height,
+                      TextFormField(
+                        controller: emailCont,
+                        focusNode: emailFocus,
+                        style: primaryTextStyle(),
+                        decoration: InputDecoration(
+                          labelText: 'Correo Electrónico',
+                          labelStyle: secondaryTextStyle(),
+                          contentPadding: EdgeInsets.all(16),
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              borderSide: BorderSide(color: appColorPrimary)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              borderSide: BorderSide(
+                                  color: appStore.textSecondaryColor!)),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        onFieldSubmitted: (s) =>
+                            FocusScope.of(context).requestFocus(passFocus),
+                        textInputAction: TextInputAction.next,
                       ),
-                      keyboardType: TextInputType.emailAddress,
-                      onFieldSubmitted: (s) =>
-                          FocusScope.of(context).requestFocus(passFocus),
-                      textInputAction: TextInputAction.next,
-                    ),
-                    16.height,
-                    TextFormField(
-                      obscureText: obscureText,
-                      focusNode: passFocus,
-                      controller: passCont,
-                      style: primaryTextStyle(),
-                      decoration: InputDecoration(
-                        labelText: 'Contraseña',
-                        contentPadding: EdgeInsets.all(16),
-                        labelStyle: secondaryTextStyle(),
-                        border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                            borderSide: BorderSide(color: appColorPrimary)),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                            borderSide: BorderSide(
-                                color: appStore.textSecondaryColor!)),
-                        suffix: Icon(!obscureText
-                                ? Icons.visibility
-                                : Icons.visibility_off)
-                            .onTap(() {
-                          obscureText = !obscureText;
-                          setState(() {});
-                        }),
+                      16.height,
+                      TextFormField(
+                        obscureText: obscureText,
+                        focusNode: passFocus,
+                        controller: passCont,
+                        style: primaryTextStyle(),
+                        decoration: InputDecoration(
+                          labelText: 'Contraseña',
+                          contentPadding: EdgeInsets.all(16),
+                          labelStyle: secondaryTextStyle(),
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              borderSide: BorderSide(color: appColorPrimary)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              borderSide: BorderSide(
+                                  color: appStore.textSecondaryColor!)),
+                          suffix: Icon(!obscureText
+                              ? Icons.visibility
+                              : Icons.visibility_off)
+                              .onTap(() {
+                            obscureText = !obscureText;
+                            setState(() {});
+                          }),
+                        ),
                       ),
-                    ),
-                    20.height,
-                    Container(
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                      decoration: BoxDecoration(
-                          color: appColorPrimary,
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: defaultBoxShadow()),
-                      child: Text('${isTaped ? 'Iniciando...' : 'Iniciar'}',
-                          style: boldTextStyle(color: white, size: 18)),
-                    ).onTap(() {
-                      finish(context);
-                      if (!isTaped) {
-                        _login();
-                      }
-
-                      /// Remove comment if you want enable validation
-                      /* if (formKey.currentState.validate()) {
-                      formKey.currentState.save();
-                      finish(context);
-                    } else {
-                      autoValidate = true;
-                    }
-                    setState(() {});*/
-                    }),
-                    /*20.height,
-                Text('Already Registered?', style: boldTextStyle(color: appColorPrimary)).center().onTap(() {
-                  finish(context);
-                }),*/
-                  ],
-                ),
-              ).center(),
+                      20.height,
+                      Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                        decoration: BoxDecoration(
+                            color: appColorPrimary,
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: defaultBoxShadow()),
+                        child: Text('${isTaped ? 'Iniciando...' : 'Iniciar'}',
+                            style: boldTextStyle(color: white, size: 18)),
+                      ).onTap(() {
+                        finish(context);
+                        if (!isTaped) {
+                          _login();
+                        }
+                      }),
+                      20.height,
+                      if(messageError != null)
+                        Text('${messageError}', style: boldTextStyle(
+                            color: appColorPrimary)).center(),
+                    ],
+                  ),
+                ).center(),
+              ),
             ),
-          ),
-        ));
+          ));
+    }
   }
-}
